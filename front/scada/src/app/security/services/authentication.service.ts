c6786a1656ac687719f1586349b62d009fba7903
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-07-01 08:37:45 +0200

    Added basic authentication services

---
+++ front/scada/src/app/security/services/authentication.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import {environment} from 'src/app/environment/environment';
+import {JwtHelperService} from '@auth0/angular-jwt'
+import {Token} from '../../models/Token'
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  user$ = new BehaviorSubject(null);
+  userState$ = this.user$.asObservable();
+
+  constructor(private http:HttpClient) {
+    this.user$.next(this.getRole());
+   }
+   isPasswordDurationValid(email: string): Observable<boolean> {
+    return this.http.get<boolean>(environment.apiHost + 'api/user/password-expired/' + email);
+  }
+
+
+   login(email: string, password: string, type: string): Observable<string> {
+    return this.http.post(environment.apiHost + 'api/user/login', { email, password, type }, { responseType: 'text' });
+  }
+  
+
+  logout(){
+    localStorage.removeItem('user');
+    this.setUser();
+
+  }
+  isLoggedIn(): boolean{
+    return localStorage.getItem('user') != null;
+  }
+
+
+  getRole():any{
+    if(this.isLoggedIn()){
+      const accessToken: string | null = localStorage.getItem('user');
+      const helper = new JwtHelperService();
+      if(accessToken != null){
+        const role = helper.decodeToken(accessToken).role[0].authority;
+        return role;
+      }
+      return null;
+      
+    }
+    return null;
+  }
+
+  setUser(): void{
+    this.user$.next(this.getRole());
+  }
+}
