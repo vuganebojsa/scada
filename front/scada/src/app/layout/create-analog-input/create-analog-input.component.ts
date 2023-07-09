6b7bcbd858e13caed1e3d7aa0d577495e4736f29
Bogdan <bogdanjanosevic@gmail.com>
2023-07-09 15:48:34 +0200

    Frontend - Implemented form for creating analog input tag

--- front/scada/src/app/layout/create-analog-input/create-analog-input.component.ts
+++ front/scada/src/app/layout/create-analog-input/create-analog-input.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TagsService } from 'src/app/services/tags.service';
 
 @Component({
   selector: 'app-create-analog-input',
@@ -8,4 +10,22 @@ import { Component } from '@angular/core';
 export class CreateAnalogInputComponent {
 
 
+  createForm = new FormGroup({
+    tagName: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    description: new FormControl('', [Validators.required]),
+    units: new FormControl('', [Validators.required]),
+    currentValue: new FormControl('', [Validators.required]),
+    scanTime: new FormControl('', [Validators.required]),
+    lowLimit: new FormControl('', [Validators.required]),
+    highLimit: new FormControl('', [Validators.required])
+
+  });
+  
+  constructor( private tagService: TagsService, ){
+
+  }
+
+  createAi():void{
+
+  }
 }
